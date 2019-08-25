a70b16a01f76bcf4553c5fdcea530b4bda13b551
Anyviel <danyvielcos@gmail.com>
2019-08-24 22:38:50 -0300

    Atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 // Adicionamos nosso código dentro de uma função anônima para evitar
 // variáveis globais.
-(function() {
+
+(function () {
   // Elemento principal do formulário
   const contactForm = document.getElementById('contact');
 
@@ -12,9 +13,32 @@
   // "addEventListener" instrui o navegador a executar uma função sempre
   // que um evento específico acontece. Nesse caso, a função abaixo será
   // executada toda vez que o botão de envio for pressionado.
-  contactForm.addEventListener('submit', function() {
+  contactForm.addEventListener('submit', function () {
+
+    //Função para verificar se o email foi digitado corretamente.
+    function isValidEmail(email) {
+      return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email)
+    }
     // A propriedade "value" contém o valor atual de um input ou textarea
     const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
+
+    //Verificação dos campos deixados em branco utilizando a condicional "if", que  
+    //é uma estrutura condicional que executa a afirmação, dentro do bloco, se 
+    //determinada condição for verdadeira.
+    if (nameValue === '') {
+      return alert('Preencha o nome do usuário');
+    }
+    if (emailValue === '') {
+      return alert('Preencha o endereço de email');
+    }
+    if (messageValue === '') {
+      return alert('Preencha a mensagem.');
+    }
+    if (!isValidEmail(emailInput.value)) {
+      return alert('Preencha com um email válido.');
+    }
 
     // A propriedade "length" contém o tamanho (em caracteres) de uma
     // uma string. Toda string contém essa propriedade.
@@ -24,7 +48,7 @@
     // navegador que está executando o código do seu site. O método
     // "alert" simplesmente mostra um aviso para o usuário contendo a
     // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
+    window.alert(`Mensagem enviada com sucesso!`);
 
     // Altere e complete essa função para validar os campos do formulário
     // de acordo com as especificações do teste. Boa sorte!
